fa9c3c6b57473821d87a858b690464516c58d895
70_10 <7010.dev@gmail.com>
2017-09-26 23:47:18 +0900

    [add] config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const path = require("path");
 // eslint-disable-next-line import/no-unresolved
 const slsw = require("serverless-webpack");
+const config = require("config");
+const fs = require("fs");
+const configFilePath = path.resolve(__dirname, "config.json");
+fs.writeFileSync(configFilePath, JSON.stringify(config));
 
 module.exports = {
   entry: slsw.lib.entries,
@@ -19,5 +23,10 @@ module.exports = {
     libraryTarget: "commonjs",
     path: path.join(__dirname, ".webpack"),
     filename: "[name].js",
+  },
+  resolve: {
+    alias: {
+      config: configFilePath,
+    },
   },
 };
